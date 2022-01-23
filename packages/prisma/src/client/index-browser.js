3ea2eb5afabc4912223c4fae2d4e049e61eee0c2
T3NED <software@t3ned.com>
2022-01-23 14:47:08 +0000

    feat(prisma): :sparkles: output prisma client

---
+++ packages/prisma/src/client/index-browser.js
@@ -0,0 +1,176 @@
+
+Object.defineProperty(exports, "__esModule", { value: true });
+
+const {
+  Decimal
+} = require('./runtime/index-browser')
+
+
+const Prisma = {}
+
+exports.Prisma = Prisma
+
+/**
+ * Prisma Client JS version: 3.8.1
+ * Query Engine version: 34df67547cf5598f5a6cd3eb45f14ee70c3fb86f
+ */
+Prisma.prismaVersion = {
+  client: "3.8.1",
+  engine: "34df67547cf5598f5a6cd3eb45f14ee70c3fb86f"
+}
+
+Prisma.PrismaClientKnownRequestError = () => {
+  throw new Error(`PrismaClientKnownRequestError is unable to be run in the browser.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)};
+Prisma.PrismaClientUnknownRequestError = () => {
+  throw new Error(`PrismaClientUnknownRequestError is unable to be run in the browser.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.PrismaClientRustPanicError = () => {
+  throw new Error(`PrismaClientRustPanicError is unable to be run in the browser.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.PrismaClientInitializationError = () => {
+  throw new Error(`PrismaClientInitializationError is unable to be run in the browser.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.PrismaClientValidationError = () => {
+  throw new Error(`PrismaClientValidationError is unable to be run in the browser.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.Decimal = Decimal
+
+/**
+ * Re-export of sql-template-tag
+ */
+Prisma.sql = () => {
+  throw new Error(`sqltag is unable to be run in the browser.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.empty = () => {
+  throw new Error(`empty is unable to be run in the browser.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.join = () => {
+  throw new Error(`join is unable to be run in the browser.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.raw = () => {
+  throw new Error(`raw is unable to be run in the browser.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
+Prisma.validator = () => (val) => val
+
+/**
+ * Shorthand utilities for JSON filtering
+ */
+Prisma.DbNull = 'DbNull'
+Prisma.JsonNull = 'JsonNull'
+Prisma.AnyNull = 'AnyNull'
+
+/**
+ * Enums
+ */
+// Based on
+// https://github.com/microsoft/TypeScript/issues/3192#issuecomment-261720275
+function makeEnum(x) { return x; }
+
+exports.Prisma.UserScalarFieldEnum = makeEnum({
+  id: 'id',
+  username: 'username',
+  discriminator: 'discriminator',
+  avatar: 'avatar',
+  sitePermissions: 'sitePermissions',
+  createdAt: 'createdAt',
+  updatedAt: 'updatedAt'
+});
+
+exports.Prisma.SessionScalarFieldEnum = makeEnum({
+  id: 'id',
+  invalid: 'invalid',
+  device: 'device',
+  accessToken: 'accessToken',
+  refreshToken: 'refreshToken',
+  token: 'token',
+  createdAt: 'createdAt',
+  expiresAt: 'expiresAt',
+  userId: 'userId'
+});
+
+exports.Prisma.ApplicationScalarFieldEnum = makeEnum({
+  id: 'id',
+  name: 'name',
+  requestCount: 'requestCount',
+  requestLimit: 'requestLimit',
+  requestLimitResetAt: 'requestLimitResetAt',
+  createdAt: 'createdAt',
+  updatedAt: 'updatedAt',
+  ownerId: 'ownerId',
+  userId: 'userId'
+});
+
+exports.Prisma.FlagScalarFieldEnum = makeEnum({
+  id: 'id',
+  name: 'name',
+  description: 'description',
+  color: 'color',
+  createdAt: 'createdAt',
+  updatedAt: 'updatedAt'
+});
+
+exports.Prisma.ReportScalarFieldEnum = makeEnum({
+  id: 'id',
+  userId: 'userId',
+  reporterUserId: 'reporterUserId',
+  flagId: 'flagId',
+  createdAt: 'createdAt',
+  updatedAt: 'updatedAt'
+});
+
+exports.Prisma.AppealScalarFieldEnum = makeEnum({
+  id: 'id',
+  reason: 'reason',
+  isSuccessful: 'isSuccessful',
+  resolvedReason: 'resolvedReason',
+  resolvedAt: 'resolvedAt',
+  resolverUserId: 'resolverUserId',
+  reportId: 'reportId',
+  createdAt: 'createdAt',
+  updatedAt: 'updatedAt'
+});
+
+exports.Prisma.SortOrder = makeEnum({
+  asc: 'asc',
+  desc: 'desc'
+});
+
+exports.Prisma.QueryMode = makeEnum({
+  default: 'default',
+  insensitive: 'insensitive'
+});
+
+
+exports.Prisma.ModelName = makeEnum({
+  User: 'User',
+  Session: 'Session',
+  Application: 'Application',
+  Flag: 'Flag',
+  Report: 'Report',
+  Appeal: 'Appeal'
+});
+
+/**
+ * Create the Client
+ */
+class PrismaClient {
+  constructor() {
+    throw new Error(
+      `PrismaClient is unable to be run in the browser.
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+    )
+  }
+}
+exports.PrismaClient = PrismaClient
+
+Object.assign(exports, Prisma)
