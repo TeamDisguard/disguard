630a8fd524497e44bc0613eccdf074fb97c16e00
T3NED <software@t3ned.com>
2022-01-23 20:21:16 +0000

    feat(api): :sparkles: add upsertUser method

--- packages/api/src/lib/services/user.service.ts
+++ packages/api/src/lib/services/user.service.ts
@@ -1,4 +1,7 @@
 import type { RESTGetAPICurrentUserResult } from "discord-api-types/rest/v9";
+import type { User } from "@disguard/prisma/src/client";
+import { Database } from "@disguard/database";
+import { SitePermissions } from "#lib";
 import { endpoints } from "#consts";
 import axios from "axios";
 
@@ -17,3 +20,30 @@ export const getSelf = async (
     return null;
   }
 };
+
+export const upsertUser = (data: UserData): Promise<User> => {
+  return Database.client().user.upsert({
+    where: {
+      id: data.id
+    },
+    create: {
+      id: data.id,
+      username: data.username,
+      discriminator: data.discriminator,
+      avatar: data.avatar,
+      sitePermissions: new SitePermissions().bitfield
+    },
+    update: {
+      username: data.username,
+      discriminator: data.discriminator,
+      avatar: data.avatar
+    }
+  });
+};
+
+export interface UserData {
+  id: string;
+  username: string;
+  discriminator: string;
+  avatar: string | null;
+}
