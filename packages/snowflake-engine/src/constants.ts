336671ac4f291098b7f727b41f0cf9f04838fca9
T3NED <software@t3ned.com>
2022-01-23 00:01:42 +0000

    feat(snowflake-engine): :sparkles: add snowflake with disguard epoch

---
+++ packages/snowflake-engine/src/constants.ts
@@ -0,0 +1,4 @@
+/**
+ * Disguard epoch timestamp - 2021-01-01 at 00:00:00.000 UTC+0
+ */
+export const DISGUARD_EPOCH = 1609459200000n;
---
+++ packages/snowflake-engine/src/lib/DisguardSnowflake.ts
@@ -0,0 +1,3 @@
+import { Snowflake, DISGUARD_EPOCH } from "../index";
+
+export const DisguardSnowflake = new Snowflake(DISGUARD_EPOCH);
