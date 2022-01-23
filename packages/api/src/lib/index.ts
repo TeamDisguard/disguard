23b2c2e3d97c756b597419ce970fe80efecf6479
T3NED <software@t3ned.com>
2022-01-23 21:06:15 +0000

    feat(api): :sparkles: register auth router

--- packages/api/src/lib/index.ts
+++ packages/api/src/lib/index.ts
@@ -1,14 +1,24 @@
 // errors
 export * from "./errors/MissingEnvVariableError";
 
-// middlewares
-export * from "./middlewares/error";
-
-// routes
-export { default as v1Router } from "./routes/v1";
-
 // utils
 export * from "./utils/ApiError";
 export * from "./utils/ApiResponse";
 export * from "./utils/ApiResponseJson";
+export * from "./utils/BitField";
+export * from "./utils/bitfields/SitePermissions";
+export * from "./utils/CatchServerError";
+export * from "./utils/GetDevice";
 export * from "./utils/Logger";
+
+// middlewares
+export * from "./middlewares/error";
+
+// services
+export * from "./services";
+
+// controllers
+export { default as AuthController } from "./controllers/auth/auth.route";
+
+// routes
+export { default as v1Router } from "./routes/v1";
--- packages/api/src/lib/routes/v1/index.ts
+++ packages/api/src/lib/routes/v1/index.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
 
+import { AuthController } from "#lib";
+
 const router = Router();
+
+router.use("/auth", AuthController);
 
 export default router;
