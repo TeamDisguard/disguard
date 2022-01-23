27242996062bc53b3daf73c15daf6a36a66160e8
T3NED <software@t3ned.com>
2022-01-23 19:48:00 +0000

    feat(api): :sparkles: add discord constants

--- packages/api/src/constants.ts
+++ packages/api/src/constants.ts
@@ -1,3 +1,28 @@
+import { OAuth2Scopes } from "discord-api-types";
+import { URL, URLSearchParams } from "url";
+import { host, clientId } from "#config";
+
+export const endpoints = {
+  authorize: "https://discord.com/api/oauth2/authorize",
+  token: "https://discord.com/api/oauth2/token",
+  redirectUri: `${host}/api/v1/auth/callback`,
+  user: (id: string) => `https://discord.com/api/v9/users/${id}`
+};
+
+export const grantTypes = {
+  authorizationCode: "authorization_code",
+  refreshToken: "refresh_token"
+};
+
+const oauth2UrlParams = new URLSearchParams({
+  client_id: clientId,
+  redirect_uri: endpoints.redirectUri,
+  response_type: "code",
+  scope: OAuth2Scopes.Identify
+});
+
+export const oauth2Url = new URL(`${endpoints.authorize}?${oauth2UrlParams}`).toString();
+
 export enum HttpCodes {
   Ok = 200,
   Created,
