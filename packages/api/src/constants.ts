84d09976b9b48267507100334a69cf8f541ebbbc
T3NED <software@t3ned.com>
2022-01-23 16:48:08 +0000

    feat(api): :sparkles: add http codes, error codes and error messages

--- packages/api/src/constants.ts
+++ packages/api/src/constants.ts
@@ -1,1 +1,39 @@
-export default {};
+export enum HttpCodes {
+  Ok = 200,
+  Created,
+  NoContent = 204,
+  BadRequest = 400,
+  Unauthorized,
+  Forbidden = 403,
+  NotFound,
+  InternalServerError = 500
+}
+
+export const errorCodes = {
+  [HttpCodes.BadRequest]: "bad-request",
+  [HttpCodes.Unauthorized]: "unauthorized",
+  [HttpCodes.Forbidden]: "forbidden",
+  [HttpCodes.NotFound]: "not-found",
+  [HttpCodes.InternalServerError]: "internal-error"
+};
+
+export const errorMessages = {
+  [HttpCodes.BadRequest]: "Bad Request",
+  [HttpCodes.Unauthorized]: "Unauthorized",
+  [HttpCodes.Forbidden]: "Forbidden",
+  [HttpCodes.NotFound]: "Not Found",
+  [HttpCodes.InternalServerError]: "Internal Server Error"
+};
+
+// Prefix error messages with http code
+for (const errorCode of Reflect.ownKeys(errorMessages)) {
+  Reflect.defineProperty(errorMessages, errorCode, {
+    value: `${errorCode.toString()}: ${Reflect.get(errorMessages, errorCode)}`
+  });
+}
+
+export default {
+  HttpCodes,
+  errorCodes,
+  errorMessages
+};
