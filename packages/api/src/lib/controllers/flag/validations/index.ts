38593250cedb902322ee1fa2f7c0deec5d605628
T3NED <software@t3ned.com>
2022-01-24 19:13:39 +0000

    fix(api): :bug: allowed non-trimmed content

--- packages/api/src/lib/controllers/flag/validations/index.ts
+++ packages/api/src/lib/controllers/flag/validations/index.ts
@@ -3,13 +3,13 @@ import joi from "joi";
 const nameRegex = /^\w[\w\s]*$/;
 
 export const createFlagSchema = joi.object({
-  name: joi.string().pattern(nameRegex).min(3).max(128).required(),
-  description: joi.string().min(3).max(255).required(),
+  name: joi.string().pattern(nameRegex).min(3).max(128).required().trim(),
+  description: joi.string().min(3).max(255).required().trim(),
   color: joi.number().integer().min(0x000000).max(0xffffff).required()
 });
 
 export const updateFlagSchema = joi.object({
-  name: joi.string().pattern(nameRegex).min(3).max(128),
-  description: joi.string().min(3).max(255),
+  name: joi.string().pattern(nameRegex).min(3).max(128).trim(),
+  description: joi.string().min(3).max(255).trim(),
   color: joi.number().integer().min(0x000000).max(0xffffff)
 });
