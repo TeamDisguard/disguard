d4d56dd4f86fa2eb1acc399781db0ae4138c925b
T3NED <software@t3ned.com>
2022-01-24 18:35:46 +0000

    feat(api): :sparkles: add create flag schema validation

---
+++ packages/api/src/lib/controllers/flag/validations/index.ts
@@ -0,0 +1,9 @@
+import joi from "joi";
+
+const nameRegex = /^\w[\w\s]*$/;
+
+export const createFlagSchema = joi.object({
+  name: joi.string().pattern(nameRegex).min(3).max(128).required(),
+  description: joi.string().min(3).max(255).required(),
+  color: joi.number().integer().min(0x000000).max(0xffffff).required()
+});
