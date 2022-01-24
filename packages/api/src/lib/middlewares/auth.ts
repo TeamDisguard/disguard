9aae09e76f0e48d81b452a66ce5171d1111a17c6
T3NED <software@t3ned.com>
2022-01-24 13:55:19 +0000

    feat(api): :sparkles: check if session has expired or is invalid

--- packages/api/src/lib/middlewares/auth.ts
+++ packages/api/src/lib/middlewares/auth.ts
@@ -4,13 +4,34 @@ import { ApiError, sessionService } from "#lib";
 import { encryptionKey } from "#config";
 import { HttpCodes } from "#consts";
 
+const isExpiredSession = async (
+  userId: string,
+  id: string,
+  expiresAt: Date
+): Promise<boolean> => {
+  if (expiresAt.getTime() > Date.now()) return false;
+  await sessionService.deleteSession(userId, id);
+  return true;
+};
+
 export const auth = async (req: Request, res: Response, next: NextFunction) => {
   const sessionToken: string = req.cookies.session ?? "";
   const [encodedUserId, version, hash] = sessionToken.split(".");
   const userId = Buffer.from(encodedUserId, "base64url").toString("utf-8");
 
   const session = await sessionService.getSession(userId, version);
-  if (!session) return next(new ApiError(HttpCodes.Unauthorized));
+
+  // Check invalid session
+  if (!session || session.invalid) {
+    return next(new ApiError(HttpCodes.Unauthorized));
+  }
+
+  /**
+   * Check expired session
+   */
+  if (await isExpiredSession(userId, session.id, session.expiresAt)) {
+    return next(new ApiError(HttpCodes.Unauthorized));
+  }
 
   const isMatchedHash = await bcryptCompare(hash, session.token);
   if (!isMatchedHash) return next(new ApiError(HttpCodes.Unauthorized));
