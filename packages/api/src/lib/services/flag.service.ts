b9d7e106858a638e7140ac72505d5491ec04b432
T3NED <software@t3ned.com>
2022-01-24 18:35:08 +0000

    feat(api): :sparkles: add flag service

---
+++ packages/api/src/lib/services/flag.service.ts
@@ -0,0 +1,40 @@
+import { Database } from "@disguard/database";
+import { DisguardSnowflake } from "@disguard/snowflake-engine";
+import { toTitleCase } from "#lib";
+
+export const getFlag = (id: string) => {
+  return Database.client().flag.findUnique({
+    where: {
+      id
+    }
+  });
+};
+
+export const getFlagByName = (name: string) => {
+  return Database.client().flag.findUnique({
+    where: {
+      name
+    }
+  });
+};
+
+export const createFlag = async (data: FlagData) => {
+  const name = toTitleCase(data.name);
+  const flag = await getFlagByName(name);
+  if (flag) return null;
+
+  return Database.client().flag.create({
+    data: {
+      id: DisguardSnowflake.generate(),
+      name,
+      description: data.description,
+      color: data.color
+    }
+  });
+};
+
+export interface FlagData {
+  name: string;
+  description: string;
+  color: number;
+}
--- packages/api/src/lib/services/index.ts
+++ packages/api/src/lib/services/index.ts
@@ -1,3 +1,4 @@
 export * as authService from "./auth.service";
+export * as flagService from "./flag.service";
 export * as sessionService from "./session.service";
 export * as userService from "./user.service";
